e1b9c796c846f9c5fd02192b2cfdaa95add94e02
kooksee@163.com <kooksee@163.com>
2018-03-29 14:15:45 +0800

    添加依赖

---
+++ vendor/github.com/tendermint/ethermint/version/version.go
@@ -0,0 +1,24 @@
+package version
+
+// Major version component of the current release
+const Major = 0
+
+// Minor version component of the current release
+const Minor = 5
+
+// Fix version component of the current release
+const Fix = 4
+
+var (
+	// Version is the full version string
+	Version = "0.5.4"
+
+	// GitCommit is set with --ldflags "-X main.gitCommit=$(git rev-parse --short HEAD)"
+	GitCommit string
+)
+
+func init() {
+	if GitCommit != "" {
+		Version += "-" + GitCommit
+	}
+}
